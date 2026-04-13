8caaa6fac38ce4c54062fda2cd0566b7dbdfdd40
Nadun Ranasingha <nadunranasingha340@gmail.com>
2026-04-13 21:12:11 +0530

    Add changes

--- server/server.js
+++ server/server.js
@@ -34,7 +34,11 @@ app.use("/api/leaves", leaveRouter)
 app.use("/api/payslips", payslipRouter)
 app.use("/api/dashboard", dashboardRouter)
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ 
+    client: inngest, 
+    functions,
+    signingKey: process.env.INNGEST_SIGNING_KEY 
+}));
 
 await connectDB()
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
